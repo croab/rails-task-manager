7a5c9052ee36c17252fdf22c065cf4e57053639a
Benjamin Croad <benjamincroadapple@gmail.com>
2022-08-23 13:02:28 +0100

    Solution to exercise 2

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,12 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get '/tasks', to: 'tasks#index', as: :tasks
+  get '/tasks/new', to: 'tasks#new', as: :new_task
+  get '/tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  get '/tasks/:id', to: 'tasks#show', as: :task
+  patch '/tasks/:id', to: 'tasks#update', as: :update_task
+  post '/tasks', to: 'tasks#create'
+  delete '/tasks/:id', to: 'tasks#destroy', as: :destroy_task
+  # resources :tasks, only: [:index, :new, :edit, :show, :update, :create, :destroy]
 end
